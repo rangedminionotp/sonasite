466c2016998ee81fafbd7f33de2e77f066d0324d
Jialin Li <jli511@ucsc.edu>
2024-05-14 15:47:56 -0700

    gmail log in kinda working :DDD

---
+++ sona/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,31 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
+    async session({ session, token, user }) {
+      session.userId = token.sub;
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
