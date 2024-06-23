a5c67cfc5dcab7518f6049307005497de34f486a
Jialin Li <jli511@ucsc.edu>
2024-06-23 13:15:26 -0700

    the version where add reviews is a modal popup, currently thinking about making it same kind of popup as skins, and make add reviews modal popup

--- sona/src/app/api/graphql/SkinReviews/resolver.ts
+++ sona/src/app/api/graphql/SkinReviews/resolver.ts
@@ -1,7 +1,7 @@
 import { Query, Resolver, Mutation, Arg, Authorized ,  Ctx} from "type-graphql"
 import type { NextApiRequest } from 'next'
 import { SkinReviewsService } from './service'
-import { SkinReviewsInfo, SkinReviewsVote, SkinReviewsData} from './schema'
+import { SkinReviewsInfo, SkinReviewsVote, SkinReviewsData,  SkinReviewsAdd} from './schema'
 
 @Resolver()
 export class SkinReviewsResolver{
@@ -12,5 +12,9 @@ export class SkinReviewsResolver{
     @Query(() => [SkinReviewsInfo])
     async getReviewsBySkinId(@Arg('skin_id') skin_id: string): Promise<SkinReviewsInfo[]> {
         return new SkinReviewsService().getReviewsBySkinId(skin_id)
+    }
+    @Mutation(() => SkinReviewsInfo)
+    async addReview(@Arg('input') input: SkinReviewsAdd): Promise<SkinReviewsInfo> {
+        return new SkinReviewsService().createReview(input)
     }
 }
