55cb4456cac9cba175337958f936cdecb62699a6
Jialin Li <jli511@ucsc.edu>
2024-07-01 23:42:27 -0700

    restructured skin schema, added rating and rating_count

--- sona/src/app/api/graphql/Skin/resolver.ts
+++ sona/src/app/api/graphql/Skin/resolver.ts
@@ -1,16 +1,12 @@
 import { Query, Resolver, Mutation, Arg, Authorized ,  Ctx} from "type-graphql"
 import type { NextApiRequest } from 'next'
 import { SkinService } from "./service"
-import { SkinInfo, SkinOverview, AvgSkinRatingInput} from "./schema"
+import { SkinInfo, SkinOverview} from "./schema"
 
 @Resolver()
 export class SkinResolver {
     @Query(() => [SkinOverview]) 
     async getAllSkins(): Promise<SkinOverview[]>{
         return new SkinService().getSkin()
-    }
-    @Mutation(()=>SkinOverview)
-    async editSkinRating(@Arg("input") input: AvgSkinRatingInput): Promise<SkinOverview>{
-        return new SkinService().editSkinRating(input)
-    }
+    } 
 }
--- sona/src/app/api/graphql/Skin/service.ts
+++ sona/src/app/api/graphql/Skin/service.ts
@@ -1,16 +1,20 @@
 import { pool } from "@/db";
 
-import { SkinInfo, SkinOverview, AvgSkinRatingInput } from "./schema";
+import { SkinInfo, SkinOverview, SkinRating } from "./schema";
 
 export class SkinService {
     public async getSkin(): Promise<SkinOverview[]> {
-        const select = `SELECT * FROM SkinItem`
+        const select = `
+            SELECT si.id, si.name, si.data, sr.rating, sr.rating_count
+            FROM SkinItem si
+            LEFT JOIN SkinRating sr ON si.id = sr.skin_id
+        `;
         const query = {
             text: select,
             values: []
-        }
-        const { rows } = await pool.query(query) 
-        const skin: SkinOverview[] = []
+        };
+        const { rows } = await pool.query(query);
+        const skin: SkinOverview[] = [];
         for (const item of rows) {
             const skinObj: SkinInfo = {
                 "lore": item.data.lore,
@@ -19,32 +23,33 @@ export class SkinService {
                 "artist": item.data.artist,
                 "threeDURL": item.data.threeDURL, 
                 "voiceActor": item.data.voiceActor
-            }
+            }; 
             const itemObj: SkinOverview = {
                 "id": item.id,
                 "name": item.name,
-                "data": skinObj, 
-                "rating": item.rating 
-            }
-            skin.push(itemObj)
+                "data": skinObj,
+                "rating": item.rating,
+                "rating_count": item.rating_count
+            };
+            skin.push(itemObj);
         }
-        return skin
+        return skin;
     }
 
-    public async editSkinRating(input: AvgSkinRatingInput): Promise<SkinOverview> {
-        const { id, rating } = input
-        const select = `UPDATE SkinItem SET rating = $2 WHERE id = $1 RETURNING *`
-        const query = {
-            text: select,
-            values: [id, rating]
-        }
-        const {rows} = await pool.query(query)
-        const skinObj: SkinOverview = {
-            "id": rows[0].id,
-            "name": rows[0].name,
-            "data": rows[0].data,
-            "rating": rows[0].rating
-        }
-        return skinObj
-    }
+    // public async editSkinRating(input: AvgSkinRatingInput): Promise<SkinOverview> {
+    //     const { id, rating } = input
+    //     const select = `UPDATE SkinItem SET rating = $2 WHERE id = $1 RETURNING *`
+    //     const query = {
+    //         text: select,
+    //         values: [id, rating]
+    //     }
+    //     const {rows} = await pool.query(query)
+    //     const skinObj: SkinOverview = {
+    //         "id": rows[0].id,
+    //         "name": rows[0].name,
+    //         "data": rows[0].data,
+    //         "rating": rows[0].rating
+    //     }
+    //     return skinObj
+    // }
 }
