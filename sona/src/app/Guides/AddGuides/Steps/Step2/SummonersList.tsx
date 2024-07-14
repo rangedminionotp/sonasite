d22304cb6aa0bee92540aeed75e2b6623c878f98
Jialin Li <jli511@ucsc.edu>
2024-07-14 03:17:53 -0700

    draggable summoners kinda working :D

--- sona/src/app/Guides/AddGuides/Steps/Step2/SummonersList.tsx
+++ sona/src/app/Guides/AddGuides/Steps/Step2/SummonersList.tsx
@@ -9,36 +9,46 @@ const DraggableItem = ({ item, index, onDragStart }) => (
   </div>
 );
 
-const DropArea = ({ onDrop, onDragOver, children }) => (
-  <div onDrop={onDrop} onDragOver={onDragOver}>
+const DropArea = ({ onDrop, onDragOver, children, id }) => (
+  <div
+    onDrop={(e) => onDrop(e, id)}
+    onDragOver={onDragOver}
+    style={{
+      minWidth: "50px",
+      minHeight: "50px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      border: "2px dashed #ccc",
+    }}
+  >
     {children}
   </div>
 );
 
 const SummonersList = ({ summonerData }) => {
-  const [summonerPair, setSummonerPair] = useState(null);
   const [addPair, setAddPair] = useState(false);
-  const [firstSummoner, setFirstSummoner] = useState(null);
-  const [secondSummoner, setSecondSummoner] = useState(null);
-  const [droppedItems, setDroppedItems] = useState([]);
   const [draggingItem, setDraggingItem] = useState(null);
+  const [droppedItems, setDroppedItems] = useState({ D: null, F: null });
 
   const handleDragStart = (e, index) => {
     setDraggingItem(
       <Image
-        src={`${summonerData[index].imageURL}`}
+        src={summonerData[index].imageURL}
         alt={summonerData[index].name}
         width={50}
         height={50}
       />
     );
-    console.log("something", summonerData[index].imageURL);
   };
 
-  const handleDrop = (e) => {
+  const handleDrop = (e, dropAreaId) => {
     e.preventDefault();
     if (draggingItem !== null) {
-      setDroppedItems([...droppedItems, draggingItem]);
+      setDroppedItems((prevItems) => ({
+        ...prevItems,
+        [dropAreaId]: draggingItem,
+      }));
       setDraggingItem(null);
     }
   };
@@ -57,14 +67,14 @@ const SummonersList = ({ summonerData }) => {
           {summonerData &&
             summonerData.map((summoner, index) => (
               <Tooltip title={summoner.description} key={summoner.id + index}>
-                <div draggable>
+                <div>
                   <h1 className="text-gray-200">{summoner.name}</h1>
                   <h1 className="text-gray-200">{summoner.cooldown}</h1>
                   <DraggableItem
                     key={index}
                     item={
                       <Image
-                        src={`${summoner.imageURL}`}
+                        src={summoner.imageURL}
                         alt={summoner.name}
                         width={50}
                         height={50}
@@ -77,34 +87,23 @@ const SummonersList = ({ summonerData }) => {
               </Tooltip>
             ))}
         </div>
-        <DropArea onDrop={handleDrop} onDragOver={handleDragOver}>
-          <h3>Drop Area</h3>
-          {droppedItems.map((item, index) => (
-            <div
-              key={index}
-              style={{
-                padding: "8px",
-                margin: "4px",
-                backgroundColor: "#e0e0e0",
-              }}
-            >
-              {item}
-            </div>
-          ))}
-        </DropArea>
         <div
           onClick={() => setAddPair(!addPair)}
-          className=" text-gray-200 p-4 rounded-lg hover:cursor-pointer"
+          className="text-gray-200 p-4 rounded-lg hover:cursor-pointer"
         >
           <AddIcon className="text-gray-200 hover:scale-120" />
         </div>
         {addPair && (
-          <div className="flex gap-2">
-            <div className="w-16 h-16 bg-gray-200 p-2 rounded-lg border-2 border-gray-800 shadow-lg flex text-center justify-center items-center ">
-              D
+          <div className="flex gap-2 mt-4">
+            <div className="w-16 h-16 bg-gray-200 p-2 rounded-lg border-2 border-gray-800 shadow-lg flex text-center justify-center items-center relative">
+              <DropArea onDrop={handleDrop} onDragOver={handleDragOver} id="D">
+                {!droppedItems.D ? "D" : droppedItems.D}
+              </DropArea>
             </div>
-            <div className="w-16 h-16 bg-gray-200 p-2 rounded-lg border-2 border-gray-800 shadow-lg flex text-center justify-center items-center ">
-              F
+            <div className="w-16 h-16 bg-gray-200 p-2 rounded-lg border-2 border-gray-800 shadow-lg flex text-center justify-center items-center relative">
+              <DropArea onDrop={handleDrop} onDragOver={handleDragOver} id="F">
+                {!droppedItems.F ? "F" : droppedItems.F}
+              </DropArea>
             </div>
           </div>
         )}
