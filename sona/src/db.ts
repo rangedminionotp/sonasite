3cbd6f6faa90c2c32f9ed8d39f038334399e010b
Jialin Li <jli511@ucsc.edu>
2024-07-13 17:13:39 -0700

    add build by @admiralmiki

--- sona/src/db.ts
+++ sona/src/db.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  host: 'localhost',
+  host: process.env.POSTGRES_HOST,
   port: 5432,
   database: process.env.POSTGRES_DB,
   user: process.env.POSTGRES_USER,
